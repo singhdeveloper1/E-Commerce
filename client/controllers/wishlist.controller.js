b13028d3f6455e2c68276727382f828a61e7a390
singhdeveloper1 <priyanshusingh874@gmail.com>
2025-04-24 13:46:29 +0530

    created the API for delete one product from wishlist and delete whole wishlist

--- client/controllers/wishlist.controller.js
+++ client/controllers/wishlist.controller.js
@@ -1,4 +1,5 @@
 import AddToWishlist from "../models/addToWishlist.model.js"
+import { errorHandler } from "../utils/errorHandler.js"
 
 //! ad to wishlist
 
@@ -38,9 +39,45 @@ export const addToWishlist = async (req, res, next)=>{
 export const getWishlist = async (req, res, next)=>{
     try {
         const wishlist = await AddToWishlist.find({userId : req.user._id}).populate("products.productId")
+
+        if(wishlist.length == 0) return next(errorHandler(404, "no product added in the wishlist"))
         res.status(200).json(wishlist)
     } catch (error) {
         console.log("get wishlist m h error", error)
+        next(error)
+    }
+}
+
+//! delete one wishlist
+
+export const deleteOneWishlist = async (req, res, next) =>{
+    try {
+        const wishlist = await AddToWishlist.findOneAndUpdate({userId : req.user._id},{
+            $pull : {products : {
+                productId : req.params.productId
+            }}
+        },{new : true})
+
+        if(wishlist.products.length == 0){
+            await AddToWishlist.findOneAndDelete({userId : req.user._id})
+        }
+        
+        res.status(200).json("product is removed from the wishlist")
+
+    } catch (error) {
+        console.log("delete one wishlist m h error", error)
+        next(error)
+    }
+}
+
+//! delete full wishlist
+
+export const deleteFullWishlist = async(req, res, next)=>{
+    try {
+     await AddToWishlist.findOneAndDelete({userId : req.user._id})
+     res.status(200).json("wishlist deleted successfully!!!")
+    } catch (error) {
+        console.log("delete full wishlist", error)
         next(error)
     }
 }
--- client/routes/wishlist.route.js
+++ client/routes/wishlist.route.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { authentication } from "../middlewares/auth.middleware.js"
-import { addToWishlist, getWishlist } from "../controllers/wishlist.controller.js"
+import { addToWishlist, deleteFullWishlist, deleteOneWishlist, getWishlist } from "../controllers/wishlist.controller.js"
 
 const router = express.Router()
 
 router.post("/addToWishlist/:productId", authentication, addToWishlist)
-router.get("/getWishlist", authentication, getWishlist)
+router.get("/viewWishlist", authentication, getWishlist)
+router.delete("/deleteOneWishlist/:productId", authentication, deleteOneWishlist)
+router.delete("/deleteFullWishlist", authentication, deleteFullWishlist)
 
 export default router
