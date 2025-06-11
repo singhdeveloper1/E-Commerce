747c4da41925738149ead09dd607aaad2388e1e7
singhdeveloper1 <priyanshusingh874@gmail.com>
2025-06-11 19:15:13 +0530

    added the code in send notification API

--- client/controllers/notification.controller.js
+++ client/controllers/notification.controller.js
@@ -35,22 +35,40 @@ export const disableNotification = async (req, res, next)=>{
 }
 
 //! send notification
-export const sendNotification = async (req, res)=>{
-    const {email, title, body} = req.body
-    try {
-        const user = await User.findOne({email})
-        if(!user?.fcmToken) return next(errorHandler(400, "notification is disabled by user!!"))
+export const sendNotification = async (req, res, next) => {
+  const { email, title, body } = req.body;
+  try {
+    if (email) {
+      const user = await User.findOne({ email });
+      if (!user?.fcmToken)
+        return next(errorHandler(400, "notification is disabled by user!!"));
+      const message = {
+        notification: { title, body },
+        token: user.fcmToken,
+      };
 
-            const message = {
-                notification  : {title, body},
-                token : user.fcmToken
-            }
+      const response = await admin.messaging().send(message);
+      res.status(200).json("notification send successfullyy", response);
+    } else {
+      const userWithFCMtoken = await User.find({ fcmToken: { $exists: true } });
 
-            const response = await admin.messaging().send(message)
-            res.status(200).json("notification send successfullyy!!!", response)
-        
-    } catch (error) {
-        console.log("send notification m h error", error)
-        next(error)
+      const tokens = userWithFCMtoken.map((user) => user.fcmToken);
+
+      if (tokens.length === 0)
+        return next(
+          errorHandler(400, "notification is disabled by all the Users!!")
+        );
+
+      const message = {
+        notification: { title, body },
+        tokens,
+      };
+
+      const response = await admin.messaging().sendEachForMulticast(message);
+      res.status(200).json("notification send successfullyy!!!", response);
     }
-}
+  } catch (error) {
+    console.log("send notification m h error", error);
+    next(error);
+  }
+};
